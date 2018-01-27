fc9d2d8efa26fef388f8f415911737db461e400f
cmirc <35879637+cmirc@users.noreply.github.com>
2018-01-27 23:47:03 +0100

    Create start0.R

---
+++ start0.R
@@ -0,0 +1,29 @@
+
+# start0
+
+# if needed:
+# .libPaths( c( .libPaths(), "~/a/b/c") )
+ 
+
+
+# in bash:
+#> sudo apt-get install libcurl4-openssl-dev libssl-dev
+
+install.packages("devtools")
+library(devtools)
+install.packages("githubinstall")
+library(githubinstall)
+# to enable easy package installing & loading:
+gh_install_packages("easypackages")
+library(easypackages)
+
+
+# MISC
+
+https://cran.r-project.org/web/packages/easypackages/vignettes/easypackages-vignette.html 
+https://rdrr.io/github/ldepascalis/packages/ 
+https://www.rdocumentation.org/packages/githubinstall/versions/0.2.1/topics/gh_install_packages
+
+https://jennybc.github.io/2014-05-12-ubc/ubc-r/session03_git.html
+
+https://stackoverflow.com/questions/17425686/how-to-install-sp-r-package-from-existing-gz-file 
